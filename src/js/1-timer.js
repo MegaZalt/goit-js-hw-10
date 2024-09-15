33f44ea8b1010a30cde1b49f2e7264aa3879942e
Valeriia <malvinacat0404@gmail.com>
2024-09-15 18:13:04 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,6 +10,9 @@ const hoursValue = document.querySelector("[data-hours]");
 const minutesValue = document.querySelector("[data-minutes]");
 const secondsValue = document.querySelector("[data-seconds]");
 
+let userSelectedDate = null;
+let timerId = null;
+
 startButton.disabled = true;
 
 flatpickr("#datetime-picker", {
