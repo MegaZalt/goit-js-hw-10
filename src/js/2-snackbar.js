6e66ab3a660a83b950f6f69f7da1900602c9b268
Valeriia <malvinacat0404@gmail.com>
2024-09-18 16:54:30 +0200

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -26,15 +26,31 @@ function createPromise(delay, state) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (state === "fulfilled") {
-                resolve(`Promise fulfilled after ${delay}ms`);
+                resolve(delay);
             } else {
-                reject(`Promise rejected after ${delay}ms`);
+                reject(delay);
             }
         }, delay);
     });
 }
 
 function showNotification(message, type) {
+    if(type === "success") {
+        iziToast.success({
+            title: "Success",
+            message: message,
+            position: 'topRight',
+        });
+    } else if(type === "error") {
+        iziToast.error({
+            title: "Error",
+            message: message,
+            position: 'topRight',
+        });
+    }
+}
+
+function showTextFcton (message, type) {
     const notificationDiv = document.getElementById("notification");
     notificationDiv.textContent = message;
     notificationDiv.className = type;
